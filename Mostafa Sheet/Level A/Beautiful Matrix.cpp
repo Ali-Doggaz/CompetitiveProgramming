4c4ef5b0090d76de71e4e1af1fd09d7ecde46da8
Ali-Doggaz <62618334+Ali-Doggaz@users.noreply.github.com>
2020-12-14 09:21:34 +0100

    Update Beautiful Matrix.cpp

--- Mostafa Sheet/Level A/Beautiful Matrix.cpp
+++ Mostafa Sheet/Level A/Beautiful Matrix.cpp
@@ -1,15 +1,26 @@
-
 #include <iostream>
 #include <math.h> 
 using namespace std;
 
 int main(){
-    double L,B;
-    cin >> L >> B;
-    double result = log(B/L) / log(1.5);
-    if (ceil(result) == result) cout << result+1;
-    else cout << ceil(result);
-    return 0;
+    int n;
+    int T[5][5];
 
+    int absc;
+    int ord;
+
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++) {
+            cin >> T[i][j];
+            if (T[i][j] == 1) {
+                absc = i;
+                ord = j;
+            }
+        }
+    }
+
+    cout << abs((absc-2))+abs(ord-2);
+
+    return 0;
 }
 
